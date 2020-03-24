0f94ec9e775fc463634db7cace8db4c6e63d0629
Eduardo Laguna <eduardo.laguna@mercadolibre.cl>
2020-03-24 01:03:05 -0300

    Uses step functions to create the thumbnail

--- handler.js
+++ handler.js
@@ -1,13 +1,15 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+const stepFunctions = new AWS.StepFunctions();
+
 const resizer = require('./resizer');
 
 module.exports.resizer = (event, context, callback) => {
-    const s3 = event.Records[0].s3;
-    console.log(s3);
+    console.log(event);
 
-    const bucket = s3.bucket.name
-    const key = s3.object.key;
+    const bucket = event.bucketName;
+    const key = event.objectKey;
 
     const message = `A file named ${key} was put in a bucket ${bucket}`;
     console.log(message)
@@ -23,6 +25,16 @@ module.exports.resizer = (event, context, callback) => {
         });
 };
 
+
+module.exports.saveImageMetadata = (event, context, callback) => {
+    console.log(event);
+
+    const bucket = event.bucketName;
+    const key = event.objectKey;
+
+    console.log('saveImageMetadata was called');
+    //TODO
+}
 module.exports.thumbnailLogger = (event, context, callback) => {
     const s3 = event.Records[0].s3;
     const bucket = s3.bucket.name
@@ -31,5 +43,42 @@ module.exports.thumbnailLogger = (event, context, callback) => {
     const message = `A file named ${key} was put in a bucket ${bucket}`;
     console.log(message)
 
-    return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    return {message: 'Go Serverless v1.0! Your function executed successfully!', event};
 };
+
+module.exports.executeStepFunction = (event, context, callback) => {
+    const stateMachineName = 'ImageProcessingMachine';
+
+    console.log('Fetching the list of available workflows');
+
+    stepFunctions
+        .listStateMachines({})
+        .promise()
+        .then(listStateMachines => {
+            console.log('Searching for the step function', listStateMachines);
+            for (let i = 0; i < listStateMachines.stateMachines.length; i++) {
+                const item = listStateMachines.stateMachines[i];
+                if (item.name.indexOf(stateMachineName) >= 0) {
+                    console.log('Found the step function');
+
+                    const eventData = event.Records[0];
+
+                    const params = {
+                        stateMachineArn: item.stateMachineArn,
+                        input: JSON.stringify({
+                            objectKey: eventData.s3.object.key,
+                            bucketName: eventData.s3.bucket.name
+                        })
+                    };
+
+                    console.log('Start execution');
+                    stepFunctions.startExecution(params).promise().then(() => {
+                        return context.succeed('OK');
+                    });
+                }
+            }
+        })
+        .catch(error => {
+            return context.fail(error);
+        });
+}
